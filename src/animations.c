c1cffc50d434b714021a568f4d4359f6a26c27de
onepopcorn <marc.feliu@gmail.com>
2024-12-29 17:04:27 +0100

    docs: added comments about animations declaration

--- src/animations.c
+++ src/animations.c
@@ -1,5 +1,10 @@
 #include "animations.h"
 
+/**
+ * Animations are a sequence of sprite frames (index) that are looped through on
+ * each frame. The last frame is marked with -1 to indicate the end of the animation.
+ *
+ */
 int Animations[] = {
     2, 3, 4, 5, 6, 7, -1,              // Player walking
     0, 0, 0, 0, 0, 1, 1, 1, 1, 1, -1,  // Player Idle
