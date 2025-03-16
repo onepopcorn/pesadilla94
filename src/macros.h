1c52d5bfd689991936173c11d87bde809c4fad2d
onepopcorn <marc.feliu@gmail.com>
2025-03-16 20:33:39 +0100

    refactor: added more macros for convenience

--- src/macros.h
+++ src/macros.h
@@ -11,4 +11,7 @@
         entity->frame = 0;             \
     }
 
+// This works because sprite is same size as tile. If not, we would need to adjust the x position to center the sprite with the tile
+#define m_snapXToGrid(entity) entity->x = (uint16_t)(entity->x + entity->sprite->width * 0.5) / TILE_SIZE * TILE_SIZE;
+
 #endif
