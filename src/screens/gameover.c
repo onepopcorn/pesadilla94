97c8f21ca782934e8e5832ea356782e62614504f
onepopcorn <marc.feliu@gmail.com>
2025-03-24 00:27:12 +0100

    feat: added game over

--- src/screens/gameover.c
+++ src/screens/gameover.c
@@ -1,11 +1,25 @@
-#include <stdint.h>
-
+#include "render/video.h"
+#include "assets.h"
 #include "screens.h"
 #include "gameState.h"
+#include "text.h"
+#include "settings/controls.h"
+#include "render/effects.h"
 
 #include "gameover.h"
 
 enum Screen gameover() {
+    waitFrames(60);
+    clearScreen();
+
+    drawText(160 - (sizeof(STR_GAMEOVER) * 4), 80, font, STR_GAMEOVER, COLOR_TRANSPARENT, 0);
+    waitVSync();
+    dumpBuffer();
+
+        fadeIn(255, 10);
+    waitFrames(60);
+    fadeToBlack(255, 10);
+
     resetGameState();
     return SCREEN_MENU;
 }
